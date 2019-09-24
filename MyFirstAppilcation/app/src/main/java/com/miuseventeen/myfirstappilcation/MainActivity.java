fd2e7690d0568dc3c1e36b8f03776fe26522acc0
unknown <irham.umam36@gmail.com>
2019-09-24 14:29:18 +0700

    original

---
+++ MyFirstAppilcation/app/src/main/java/com/miuseventeen/myfirstappilcation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.miuseventeen.myfirstappilcation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
